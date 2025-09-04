44f450b2546483e11d44bf9d182eca7e07c1ac0b
miyaparker <karen@cchub.africa>
2025-09-04 19:43:36 +0100

    remove favicon from head

--- layout.tsx
+++ layout.tsx
@@ -12,9 +12,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.svg" />
-      </head>
       <body>
         <ThemeProvider theme={baselightTheme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
